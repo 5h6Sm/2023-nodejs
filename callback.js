9711f93d430b6458ba6bd366e267a6d2fd25513e
2023_spring <5h6sm.github@gmail.com>
2023-03-22 09:57:11 +0900

    null vs undefined

---
+++ callback.js
@@ -0,0 +1,3 @@
+somefunc(function(){
+    console.log("콜백함수 실행");
+})
---
+++ 동등비교.js
@@ -0,0 +1,10 @@
+//동등연산자 값만 비교
+console.log(1 === 1);
+// 일치연산자 자료형까지 비교
+console.log(1 === '1');
+
+console.log('hello' == 'hello')
+console.log('hello' === 'hello');
+
+console.log(0 == false);
+console.log(0 === false);

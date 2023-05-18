ea94494cb0caf8fc65a94b837e09dbe7427a19ad
5h6sm <5h6sm.github@gmail.com>
2023-05-18 15:01:15 +0900

    ejs food-food

--- 문법/grace/template.js
+++ 문법/grace/template.js
@@ -4,8 +4,9 @@ const path = require('path');
 const ejs = require('ejs');
 
 const index = {
-    name : 'SOOM',
-    here : '한국'
+    food1 : '내가 만든 쿠키',
+    food2 : '미역국',
+    food3 : '음료수'
 };
 
 const server = http.createServer(function(req, res){
@@ -16,7 +17,7 @@ const server = http.createServer(function(req, res){
         // fs.createReadStream(path.join(__dirname, 'html', 'index.html')).pipe(res);
         //renderfile(ejs대상경로, ejs에 넘겨줄 데이터, 이후에 실행할 callback함수)
         ejs.renderFile(path.join(__dirname, 'template', 'index.ejs'),
-        {name:index.name, here : index.here} //ejs 인자 전달\
+        {food1,food2,food3} //ejs 인자 전달\
         //데이터를 넘겨받은 ejs 코드(data)를 클라이언트에게 보낸다.
     ). then((data)=> res.end(data));
         // function(err, data){
